3f0cf108e569b8ec6a59aa6a32892c22be9d38f0
hackyrupesh <rupesh.ttf@gmail.com>
2024-09-07 11:12:10 +0530

    add code into js learn codedatabase

---
+++ 00_Hello_world/app.js
@@ -0,0 +1,3 @@
+console.log("hello");
+console.log(23 + 7);
+console.log(5+6*2+1*3/5);
---
+++ 01_dataTypes/app.js
@@ -0,0 +1,15 @@
+// there is 2 type of data types
+// primitive
+// non-primitive
+// there are 7 sub part of primitive data types
+// number         // 3+4, NaN
+// string        //  "hello"
+// boolean      // true/false
+// undefined    //  not available
+// null         // null, object
+// bigint       //
+// symbol       //
+
+
+
+console.log(BigInt(5555555555555555555555555555));
